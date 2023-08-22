2e12f6e33506ef5754585985929d7026407c8f2f
Daniel Portela <danielportela.ti@gmail.com>
2023-08-22 18:47:53 -0300

    projeto concluido

---
+++ wiki-urls.js
@@ -0,0 +1,10 @@
+const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+
+// list=search - executa uma pesquisa de texto completo
+// srsearch="inputValue" - pesquisa por títulos de página ou conteúdo correspondente a esse valor.
+// srlimit=20 Quantas páginas totais retornar.
+// formato=json resposta json
+// "origin=*" corrige erros de cors
+
+const page_url = 'href=http://en.wikipedia.org/?curid=${pageid}';
